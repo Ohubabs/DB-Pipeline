a2fefeff01a82c895dc93f9155559bd98af10ce4
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-10-03 08:24:56 -0700

    Update var.tf

--- kamehouse/var.tf
+++ kamehouse/var.tf
@@ -16,40 +16,5 @@ variable "specialbeamcanon" {
 variable "masenko" {
     description = "Password for Jenkins"
     default = "" 
-}
-
-/*variable "konoha" {
-    description = "Username for Elastic/Kibana Dashboard"
-    default = "elastic" 
-}
-
-variable "burger" {
-    description = "Password for Elastic/Kibana Dashboard"
-    default = "" 
-}
-
-variable "garlickgun" {
-    description = "Username for Grafana Dashboard"
-    default = "" 
-}
-
-variable "finalflash" {
-    description = "Password for Grafana Dashboard"
-    default = "" 
-}
-
-variable "bbq_turkey" {
-    description = "Root Password for mongodb"
-    default = "" 
-}
-
-variable "nigerian" {
-    description = "Username for springapp mongo db access"
-    default = "" #Always makes sure this matches the root user or custom user for the mongo database in the mongo helm chart
-}
-
-variable "beef_suya" {
-    description = "Password for springapp mongo db access"
-    default = "" 
 }*/
 
